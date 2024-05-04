2e778d1104ce3908c2d016af83e5c3c0852f6fd2
Krzysztof Ciszewski <krzysztof@ciszew.ski>
2024-05-04 23:53:26 +0200

    fixed config path

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,19 @@
-use std::fs;
+use log::debug;
+use std::{env, fs};
 
 use serde::{Deserialize, Serialize};
 use simplelog::info;
 use toml::{value::Table, Value};
 
-const CONFIG_PATH: &str = "config.toml";
+const CONFIG_FILENAME: &str = "config.toml";
+
+static mut CONFIG_PATH: String = String::new();
 pub trait Config<'de>: Serialize + Deserialize<'de> + Default {}
 
 pub fn get_config<'de, T: Config<'de>>(key: &str) -> T {
     ensure_file_exists();
 
-    let config: Value = toml::from_str(&fs::read_to_string(CONFIG_PATH).unwrap()).unwrap();
+    let config: Value = toml::from_str(&fs::read_to_string(get_config_path()).unwrap()).unwrap();
     let value = config.get(&key);
 
     if value.is_none() {
@@ -24,12 +27,29 @@ pub fn update_config<'de, T: Config<'de>>(key: &str, config: &T) {
     ensure_file_exists();
 
     let mut whole_config: Table =
-        toml::from_str(&fs::read_to_string(CONFIG_PATH).unwrap()).unwrap();
+        toml::from_str(&fs::read_to_string(get_config_path()).unwrap()).unwrap();
 
     whole_config.insert(key.to_string(), Value::try_from(config).unwrap());
 
     save_config_file(&whole_config);
     info!("Config updated");
+}
+
+fn get_config_path() -> String {
+    unsafe {
+        if CONFIG_PATH.is_empty() {
+            let mut path = env::current_exe()
+                .expect("Executable should have a path")
+                .parent()
+                .unwrap()
+                .to_path_buf();
+            path.push(CONFIG_FILENAME);
+            CONFIG_PATH = path.to_str().unwrap().to_string();
+            debug!("config path: \"{}\"", CONFIG_PATH);
+        }
+
+        CONFIG_PATH.clone()
+    }
 }
 
 fn initialize_default_config_key<'de, T: Config<'de>>(key: &str) -> T {
@@ -41,11 +61,11 @@ fn initialize_default_config_key<'de, T: Config<'de>>(key: &str) -> T {
 }
 
 fn save_config_file<T: ?Sized + Serialize>(contents: &T) {
-    fs::write(CONFIG_PATH, toml::to_string(&contents).unwrap()).unwrap();
+    fs::write(get_config_path(), toml::to_string(&contents).unwrap()).unwrap();
 }
 
 fn ensure_file_exists() {
-    if fs::metadata(CONFIG_PATH).is_err() {
-        fs::write(CONFIG_PATH, "").unwrap();
+    if fs::metadata(get_config_path()).is_err() {
+        fs::write(get_config_path(), "").unwrap();
     }
 }
